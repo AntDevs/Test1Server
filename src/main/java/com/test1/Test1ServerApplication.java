5f08b90012adc596c32d8c9d5c61139e292bbeae
AntDevs <gurevichDevs@gmail.com>
2022-02-05 11:09:26 +0200

    Init

---
+++ src/main/java/com/test1/Test1ServerApplication.java
@@ -0,0 +1,13 @@
+package com.test1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1ServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/test1/controllers/TestControllers.java
@@ -0,0 +1,14 @@
+package com.test1.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestControllers {
+
+	@GetMapping("/test")
+    public String home() {
+		System.out.print("test Called");
+		return "HI";
+    }
+}
