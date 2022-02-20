2a458c9efe9863cedf63bc794c7d7661bc9b0cf3
Ant <98689095+AntDevs@users.noreply.github.com>
2022-02-20 14:44:13 +0200

    Update TestControllers.java
    downloadFile

--- src/main/java/com/test1/controllers/TestControllers.java
+++ src/main/java/com/test1/controllers/TestControllers.java
@@ -6,9 +6,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestControllers {
 
-	@GetMapping("/test")
+    @GetMapping("/test")
     public String home() {
 		System.out.print("test Called");
 		return "HI";
     }
+	
+    @RequestMapping(value = "/downloads/{fileName:.+}", method = RequestMethod.GET)
+    public void downloadFile(
+            @PathVariable("fileName") String fileName,
+            //@PathVariable("fileExt") String fileExt,
+            HttpServletResponse response) throws IOException {
+        HashMap traceParam = new HashMap();
+
+        SALogger.trace(traceParam, "Start fileName:" + fileName);
+
+        try {
+
+            String src= logConfig.getDownloadSourcePath() + "/" + fileName;
+            SALogger.trace(traceParam, "Open file:" + src);
+            if ( new File(src).exists() ) {
+                String[] fileDtl = fileName.split(Pattern.quote("."));
+                String fileExt = "." + fileDtl[fileDtl.length-1];
+                response.addHeader("Content-disposition", "attachment; filename=" + fileName);
+                response.setContentType("application/" + fileExt);
+                InputStream is = new FileInputStream(src);
+                IOUtils.copy(is, response.getOutputStream());
+                response.flushBuffer();
+            } else {
+                String hostname = InetAddress.getLocalHost().getHostName();
+                response.setStatus(404);
+                response.getWriter().append("File " + src + " not found on " + hostname );
+            }
+            SALogger.trace(traceParam, "End fileName:" + fileName);
+        } catch ( Exception e){
+            e.printStackTrace();
+            SALogger.trace(traceParam, "Err:" + e.getMessage());
+        }
+
+    }
+	
 }
